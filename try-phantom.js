4f65c92be34d7e8f609572dc123d051ab40494d2
Dan Rathbone <dan.rathbone@socrata.com>
2012-05-08 18:37:13 -0700

    setting viewport

--- try-phantom.js
+++ try-phantom.js
@@ -2,12 +2,14 @@ var phantom = require("phantom")
 
 phantom.create(function(ph){
   ph.createPage(function(page){
-    page.open("http://danrathbone.com", function(status){
+    console.log(page)
+    page.set("viewportSize",{ width: 1200, height: 800 })
+    page.open("http://danrathbone.com/blog/", function(status){
       console.log(status)
       page.evaluate(function(){return document.title}, function(result){
         console.log(result)
+        page.render("dr.png", function(){console.log("rendered")})
       })
-      page.render("dr.png", function(){console.log("rendered")})
     })
   })
 })
