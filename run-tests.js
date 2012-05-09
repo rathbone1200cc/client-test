9eed12d6ced5adcc0e8e9caf395bf6e0f3766e76
Dan Rathbone <dan.rathbone@socrata.com>
2012-05-08 18:16:38 -0700

    perf test a little more usable

--- run-tests.js
+++ run-tests.js
@@ -1,4 +1,6 @@
 #!/usr/local/bin/node
-var request_urls = require("./lib/request_urls.js")
+var request_urls = require("./lib/request_urls.js"),
+    assert = require("assert")
+assert.ok(process.argv.length === 3, "usage: ./run-tests.js <input file>")
 console.log("hello, I'm running tests")
 request_urls.run(function(){console.log("done with test")})
