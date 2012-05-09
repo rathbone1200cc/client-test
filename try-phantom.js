4d7256de95d76d0e5881135a9136b2c2a35c7b2b
Dan Rathbone <dan.rathbone@socrata.com>
2012-05-09 14:00:50 -0700

    using more phantom methods

--- try-phantom.js
+++ try-phantom.js
@@ -2,14 +2,16 @@ var phantom = require("phantom")
 
 phantom.create(function(ph){
   ph.createPage(function(page){
-    console.log(page)
     page.set("viewportSize",{ width: 1200, height: 800 })
-    page.open("http://danrathbone.com/blog/", function(status){
-      console.log(status)
-      page.evaluate(function(){return document.title}, function(result){
-        console.log(result)
-        page.render("dr.png", function(){console.log("rendered")})
-      })
+    page.set("onConsoleMessage", function(msg){console.log(msg)})
+    page.set("onLoadStarted", function(status){console.log('started loading')})
+    page.open("https://opendata.test-socrata.com/", function(status){
+      console.log("page request status is " + status)
+      page.evaluate(
+        function(){console.log("returning title"); return document.title}, 
+        function(result){console.log(result)}
+      )
+      page.render("screenshot.png", function(){console.log("rendered")})
     })
   })
 })
