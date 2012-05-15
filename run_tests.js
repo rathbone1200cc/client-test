f90900365a187c94ebcab94c2491b46aab17e132
Dan Rathbone <dan.rathbone@socrata.com>
2012-05-15 16:29:49 -0700

    combining phantom functions with main test

--- run_tests.js
+++ run_tests.js
@@ -3,4 +3,4 @@ var request_urls = require("./lib/request_urls.js"),
     assert = require("assert")
 assert.ok(process.argv.length === 3, "usage: run-tests.js <input file>")
 console.log("hello, I'm running tests")
-request_urls.run(function(){console.log("done with test")})
+request_urls.run(process.argv[2], function(){console.log("done with test")})
