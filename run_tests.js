56f41910bd5d5bb52abac7129a6049293e15d164
Dan Rathbone <dan.rathbone@socrata.com>
2012-05-18 13:56:10 -0700

    added a real custom assert

--- run_tests.js
+++ run_tests.js
@@ -1,6 +1,7 @@
 #!/usr/local/bin/node
 var clientTest = require("./lib/clientTest.js"),
     assert = require("assert")
-assert.ok(process.argv.length === 3, "usage: run-tests.js <input file>")
 console.log("hello, I'm running tests")
+console.log(process.argv)
+assert.ok(process.argv.length === 3, "usage: run-tests.js <input file>")
 clientTest.run(process.argv[2], function(){console.log("done with test")})
