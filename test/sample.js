e95b5d41819d4b41b2f992a24e2006f7245963b3
Dan Rathbone <dan.rathbone@socrata.com>
2012-05-18 15:35:46 -0700

    shims to mocha work a little better

--- test/sample.js
+++ test/sample.js
@@ -2,10 +2,11 @@ var assert = require('assert'),
     clientTest = require('../lib/clientTest.js'),
     _ = require('underscore')
 
-describe('client test', function(){
-  var file = './input/staging_html.tsv'
-  describe(file, function(){
+describe('injecting client tests', function(){
+  var files = ['./input/staging_html.tsv',
+               './input/staging_json.tsv']
+  _.each(files, function(file){ describe(file, function(){
     var tests = clientTest.makeTests(file)
     _.each(tests, function(test){it(test.description, function(done){test(done)})})
-  })
+  })})
 })
