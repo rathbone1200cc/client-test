f743a0cebf3ee5d942320c3e9303fe24dfd8d9ca
Dan Rathbone <dan.rathbone@socrata.com>
2012-05-21 12:14:17 -0700

    separating drivers, allowing driver shortcutting, new concurrency option, all test actions now optional.  Behavior with no test actions is undefined

--- run_tests.js
+++ run_tests.js
@@ -3,5 +3,7 @@ var clientTest = require("./lib/clientTest.js"),
     assert = require("assert")
 console.log("hello, I'm running tests")
 console.log(process.argv)
-assert.ok(process.argv.length === 3, "usage: run-tests.js <input file>")
-clientTest.run(process.argv[2], function(){console.log("done with test")})
+var usage = "usage: run-tests.js <input file> <concurrency>"
+assert.ok(process.argv.length === 4, usage)
+var options = {concurrency:parseInt(process.argv[3])}
+clientTest.run(process.argv[2], options, function(){console.log("done with test")})
