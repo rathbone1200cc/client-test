7b74ab1cb70ec939ec427e9aa15831b8bd6fedd1
Dan Rathbone <dan.rathbone@socrata.com>
2012-06-29 14:08:14 -0700

    adding input and small fixes for perf tests

--- test/sample.js
+++ test/sample.js
@@ -3,8 +3,7 @@ var assert = require('assert'),
     _ = require('underscore')
 
 describe('injecting client tests', function(){
-  var files = ['./input/staging_html.csv',
-               './input/staging_json.csv']
+  var files = ['./input/staging_soql_perf.csv']
   _.each(files, function(file){ describe(file, function(){
     var tests = clientTest.makeTests(file)
     _.each(tests, function(test){it(test.description, function(done){test(done)})})
